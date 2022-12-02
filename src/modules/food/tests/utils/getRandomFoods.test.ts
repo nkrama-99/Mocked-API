40195b1107ea17b112413a53db7b191a1a0429a0
Abhinav Sanjeeva Prasad <abhinavsanjeevaprasad@Abhinavs-MacBook-Pro.local>
2022-12-02 18:54:53 -0500

    Added the util test for the dairy end point.

--- src/modules/food/tests/utils/getRandomFoods.test.ts
+++ src/modules/food/tests/utils/getRandomFoods.test.ts
@@ -1,10 +1,18 @@
 import getRandomFoods from '../../utils/getRandomFoods';
 import FoodEnum from '../../consts/FoodEnum';
 
-describe('get random foods', () => {
+describe('get random fruits', () => {
     it('should return a list of random fruit items', () => {
         const amount = 4;
         const res = getRandomFoods(FoodEnum.Fruits, 4);
+        expect(res.length).toBe(amount);
+    });
+});
+
+describe('get random dairy', () => {
+    it('should return a list of random dairy items', () => {
+        const amount = 4;
+        const res = getRandomFoods(FoodEnum.Dairy, 4);
         expect(res.length).toBe(amount);
     });
 });
